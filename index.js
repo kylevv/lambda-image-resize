c41163c86c0254bf9f453eb2f27be2974f421ef1
Kyle Van Vleck <kylevanvleck@gmail.com>
2019-01-05 11:12:07 -0800

    Config resize to not crop non-thumbnail images

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict'
 
-console.log('Before everything!')
 const AWS = require('aws-sdk')
-console.log('Got AWS')
 const S3 = new AWS.S3({
   signatureVersion: 'v4'
 })
@@ -12,15 +10,14 @@ const BUCKET = process.env.BUCKET
 const URL = process.env.URL
 const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/)) : new Set([])
 const ORIGINAL_PREFIX = process.env.ORIGINAL_PREFIX
-console.log('After environment setup')
 
 exports.handler = function (event, context, callback) {
   const key = event.queryStringParameters.key
   console.log('KEY:', key)
-  const match = key.match(/((\d+)x(\d+))\/(.*)/)
+  const match = key && key.match(/((\d+)x(\d+))\/(.*)/)
 
-  // Check if requested resolution is allowed
-  if (ALLOWED_RESOLUTIONS.size !== 0 && !ALLOWED_RESOLUTIONS.has(match[1])) {
+  // Check if valid query and if requested resolution is allowed
+  if (!!match && ALLOWED_RESOLUTIONS.size !== 0 && !ALLOWED_RESOLUTIONS.has(match[1])) {
     callback(null, {
       statusCode: '403',
       headers: {},
@@ -31,11 +28,12 @@ exports.handler = function (event, context, callback) {
 
   const width = parseInt(match[2], 10)
   const height = parseInt(match[3], 10)
+  const fit = match[1] === '150x100' ? 'cover' : 'contain'
   const originalKey = `${ORIGINAL_PREFIX}/${match[4]}`
 
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
-      .resize(width, height)
+      .resize(width, height, { fit })
       .toFormat('png')
       .toBuffer()
     )
